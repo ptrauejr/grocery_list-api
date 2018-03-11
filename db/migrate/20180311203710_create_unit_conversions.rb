50e3e99966764f5fc23b58269514a9a88ab7dc5b
Paul Traue <ptrauejr@gmail.com>
2018-03-11 15:39:31 -0500

    Adds UnitConversions Model

---
+++ db/migrate/20180311203710_create_unit_conversions.rb
@@ -0,0 +1,12 @@
+class CreateUnitConversions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :unit_conversions do |t|
+      t.numeric :unit1_id
+      t.numeric :unit1_quantity
+      t.numeric :unit2_id
+      t.numeric :unit2_quantity
+
+      t.timestamps
+    end
+  end
+end
